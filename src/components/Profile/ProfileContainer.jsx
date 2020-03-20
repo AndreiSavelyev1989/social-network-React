ddfbe0ff9ef5a40908ae5680677c87c647a5b6fb
Andrei <andreiscorpion19892010@gmail.com>
2020-03-20 22:43:12 +0200

    setState, local state

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -39,5 +39,4 @@ let mapStateToProps = (state) => ({
 export default compose(
   connect(mapStateToProps, { getUserProfile }),
   withRouter,
-  withAuthRedirect
 )(ProfileContainer)
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus';
 
 
 const ProfileInfo = (props) => {
@@ -8,17 +9,17 @@ const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />
   }
-  
+
   let lookingJobImage = () => {
     let jobImage = props.profile.lookingForAJob;
-    return jobImage 
-    ? <img className={styles.jobImage} src='https://img2.freepng.ru/20180519/kse/kisspng-smiley-veselyy-photography-5b007126476a15.4166329615267556222925.jpg' /> 
-    : <img className={styles.jobImage} src='https://png.pngtree.com/png-clipart/20190520/original/pngtree-angry-emoji-vector-icon-png-image_3720389.jpg' />
- }
+    return jobImage
+      ? <img className={styles.jobImage} src='https://img2.freepng.ru/20180519/kse/kisspng-smiley-veselyy-photography-5b007126476a15.4166329615267556222925.jpg' />
+      : <img className={styles.jobImage} src='https://png.pngtree.com/png-clipart/20190520/original/pngtree-angry-emoji-vector-icon-png-image_3720389.jpg' />
+  }
 
   return (
     <div>
-      <img src='http://oboi-dlja-stola.ru/file/18958/760x0/16:9/%D0%9C%D0%B0%D0%B3%D0%B8%D1%8F-%D0%B2-%D0%B1%D1%83%D1%82%D1%8B%D0%BB%D0%BA%D0%B5.jpg' />
+      {/* <img src='http://oboi-dlja-stola.ru/file/18958/760x0/16:9/%D0%9C%D0%B0%D0%B3%D0%B8%D1%8F-%D0%B2-%D0%B1%D1%83%D1%82%D1%8B%D0%BB%D0%BA%D0%B5.jpg' /> */}
 
       <div className={styles.descriptionBlock}>
         <img src={props.profile.photos.large} />
@@ -36,6 +37,8 @@ const ProfileInfo = (props) => {
         <div>fullName: {props.profile.fullName}</div>
         <div>userId: {props.profile.userId}</div>
       </div>
+
+      <ProfileStatus status={'Hello World!!!'} />
     </div>
   )
 }
