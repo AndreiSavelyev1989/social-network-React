cb2eb4355a032106c33129d237126a1fb67e7260
Andrei <andreiscorpion19892010@gmail.com>
2020-03-30 13:40:17 +0200

    redux-form

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -5,17 +5,19 @@ import sidebarReducer from "./sidebarReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
-let redusers = combineReducers(
+let rootReducer = combineReducers(
     {
         profilePage : profileReducer,
         dialogsPage : dialogsReducer,
         sidebar : sidebarReducer,
         usersPage: usersReducer,
-        auth: authReducer
+        auth: authReducer,
+        form: formReducer,
     }
 )
-let store = createStore(redusers, applyMiddleware(thunkMiddleware));
+let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
