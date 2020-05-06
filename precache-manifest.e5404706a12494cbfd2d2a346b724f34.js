0c87ea559274ffa3da959fefd7f7d80f3ab643ce
Andrei <andreiscorpion19892010@gmail.com>
2020-05-06 14:12:28 +0200

    Updates

---
+++ precache-manifest.e5404706a12494cbfd2d2a346b724f34.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e74bc1906132797af1a7729a0cef40d",
+    "url": "/AndreiSavelyev1989/social-network-React/index.html"
+  },
+  {
+    "revision": "c8fc4302f59914caf4cf",
+    "url": "/AndreiSavelyev1989/social-network-React/static/css/3.1ebb110f.chunk.css"
+  },
+  {
+    "revision": "cc205f2651c3054c7bec",
+    "url": "/AndreiSavelyev1989/social-network-React/static/css/4.fe2e3b1d.chunk.css"
+  },
+  {
+    "revision": "02ed2a07e078b8cf91f2",
+    "url": "/AndreiSavelyev1989/social-network-React/static/css/5.b7c3a574.chunk.css"
+  },
+  {
+    "revision": "27b0b5922e6b6fe6e5f4",
+    "url": "/AndreiSavelyev1989/social-network-React/static/css/main.a89aca60.chunk.css"
+  },
+  {
+    "revision": "28fc4fb5a5996ef0276f",
+    "url": "/AndreiSavelyev1989/social-network-React/static/js/2.c2c2e989.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/AndreiSavelyev1989/social-network-React/static/js/2.c2c2e989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8fc4302f59914caf4cf",
+    "url": "/AndreiSavelyev1989/social-network-React/static/js/3.604e8b20.chunk.js"
+  },
+  {
+    "revision": "cc205f2651c3054c7bec",
+    "url": "/AndreiSavelyev1989/social-network-React/static/js/4.467c0b7e.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/AndreiSavelyev1989/social-network-React/static/js/4.467c0b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ed2a07e078b8cf91f2",
+    "url": "/AndreiSavelyev1989/social-network-React/static/js/5.6f14795a.chunk.js"
+  },
+  {
+    "revision": "27b0b5922e6b6fe6e5f4",
+    "url": "/AndreiSavelyev1989/social-network-React/static/js/main.cbd188b0.chunk.js"
+  },
+  {
+    "revision": "0dd66b0961690bebf596",
+    "url": "/AndreiSavelyev1989/social-network-React/static/js/runtime-main.2af36376.js"
+  },
+  {
+    "revision": "4809d1ffd7bf8834ae716dec2e73aca0",
+    "url": "/AndreiSavelyev1989/social-network-React/static/media/preloader.4809d1ff.svg"
+  },
+  {
+    "revision": "9c5cb55ed2d23930c87ed7bd05db6f43",
+    "url": "/AndreiSavelyev1989/social-network-React/static/media/user.9c5cb55e.png"
+  }
+]);
