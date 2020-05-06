11351dae4af3158b673742ec87241797df3f90cb
Andrei <andreiscorpion19892010@gmail.com>
2020-05-06 14:19:12 +0200

    deploy our project to github pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Route, withRouter } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import UsersContainer from './components/Users/UsersContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import { connect } from 'react-redux';
@@ -19,6 +18,7 @@ import { withSuspense } from './hoc/withSuspense';
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
 
 class App extends Component {
   componentDidMount() {
@@ -38,7 +38,7 @@ class App extends Component {
         <div className='app-wrapper-content'>
           <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)} />
           <Route path='/dialogs' render={withSuspense(DialogsContainer)} />
-          <Route path='/users' render={() => <UsersContainer />} />
+          <Route path='/users' render={withSuspense(UsersContainer)} />
           <Route path='/login' render={() => <Login />} />
           <Route path='/news' component={News} />
           <Route path='/music' component={Music} />
--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,11 +1,16 @@
 
 import  React  from 'react';
 import preloader from '../../../assets/images/preloader.svg';
-
+import styles from './Preloader.module.css';
 
 const Preloader = (props) => {
 
-    return <img src={preloader} />  
+    return <div>
+        <img src={preloader} className={styles.preloader}/>
+        <span className={styles.preloaderText} >Loading... </span>
+        
+    </div>
+     
 
 }
 
